c452c10be7bfc4854b8544c87a98c06b232d4b08
sidhushesank <sidhusheshank@gmail.com>
2025-10-19 20:40:58 +0530

    Fixed drizzle config for Vercel deployment

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
-export default {
+export default defineConfig({
   schema: "./db/schema.ts",
-  out: "./db/migrations",
+  out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL ?? "",
+    url: process.env.DATABASE_URL!,
   },
-} satisfies Config;
+});
